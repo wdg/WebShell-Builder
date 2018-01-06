fdbae56f9b92ac315fbe35acf2ef7365fbb86dea
Wesley de Groot <wes@vista.aero>
2018-01-06 17:14:00 +0100

    First Commit

---
+++ Webshell Builder/Webshell Builder/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Webshell Builder
+//
+//  Created by Wesley de Groot on 06/01/2018.
+//  Copyright © 2018 Wesley de Groot. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
